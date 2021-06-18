98ee8f96ca184829ebfdfaf1cc0ff27a95eb2457
Shiki-Himura <athaus@hotmail.de>
2021-06-18 13:45:45 +0200

    Rename MainWindow => Play_Field.xaml.cs Add Difficulty.xaml.cs Update Play_Field.xaml.cs

---
+++ TicTacToe-Csharp/Difficulty_Window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TicTacToe_Csharp
+{
+    /// <summary>
+    /// Interaktionslogik für Difficulty_Window.xaml
+    /// </summary>
+    public partial class Difficulty_Window : Window
+    {
+        public Difficulty_Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
